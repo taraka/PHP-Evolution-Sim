2a90fafd2baf579df2b6a294ee4f36538495646e
Tom Rawcliffe <git@tomrawcliffe.co.uk>
2011-08-16 13:37:32 +0100

    Now have a working implementation of the evolution process will work on more complex modes now.

--- sex.php
+++ sex.php
@@ -1,8 +1,9 @@
 <?php
 define('TARGET', 'METHINKS IT IS LIKE A WEASEL');
-define('MUTATION_CHANCE', 0.04);
-define('POPULATION_SIZE', 10);
+define('MUTATION_CHANCE', 0.02);
+define('POPULATION_SIZE', 50);
 define('MAX_AGE', 3);
+define('NUM_TO_KILL', 6);
 
 class Organism
 {
@@ -19,15 +20,22 @@ class Organism
 
 	private function getRandomGenes()
 	{
+		$return = '';
+		for ($i = 0; $i < strLen(TARGET); $i++) {
+			$return .= $this->randomGene();
+		}
+		
+		return $return;
+	}
+	
+	private function randomGene()
+	{
 		static $possibleGenes;
 		if (!isset($possibleGenes)){
 			$possibleGenes = array_merge(range('A', 'Z'), array(' '));
 		}
 		
-		for ($i = 0; $i < strLen(TARGET); $i++)
-		{
-			$population[$i] .= $possibleGenes[array_rand($possibleGenes)];
-		}
+		return $possibleGenes[array_rand($possibleGenes)];
 	}
 	
 	private function calculateFitness()
@@ -54,6 +62,29 @@ class Organism
 	{
 		$this->_age++;
 	}
+	
+	public function shag(Organism $mate)
+	{
+		$child  = '';
+		$length = strLen(TARGET);
+		$female = str_split($mate->getGenes());
+		$male   = str_split($this->getGenes());
+		$mutationChance = intval(1/MUTATION_CHANCE);
+
+		for ($i = 0; $i < $length; $i++)
+		{
+			if (rand(0,$mutationChance) == 0){
+				$gene = $this->randomGene();
+			} else if(rand(0, 10) < 6) {
+				$gene = $male[$i];
+			} else {
+				$gene = $female[$i];
+			}
+			$child .= $gene;
+		}
+		
+		return new Organism($child);
+	}
 }
 
 class Population
@@ -61,71 +92,97 @@ class Population
 	private
 		$_population = array();
 		
-	public function __construct($size)
+	public function __construct()
 	{
-		for ($i = 0; $i < $size; $i++) {
+		for ($i = 0; $i < POPULATION_SIZE; $i++) {
 			$this->_population[] = new Organism();
+		}
+	}
+	
+	private function sortByFitness()
+	{
+		usort($this->_population, function (Organism $a, Organism $b) {
+			if ($a->getFitness() == $b->getFitness()) {
+		        return 0;
+		    }
+		    return ($a->getFitness() > $b->getFitness()) ? -1 : 1;
+		});
+	}
+	
+	public function getFittest()
+	{
+		return array_slice($this->_population, -2);
+	}
+	
+	public function cull()
+	{
+		foreach ($this->_population as $key => $organism)
+		{
+			if ($organism->getAge() > MAX_AGE) {
+				unset($this->_population[$key]);
+			}
+		}
+		
+		for ($i=0; $i < NUM_TO_KILL; $i++) {
+			array_shift($this->_population);
+		}
+	}
+	
+	public function getPopulationCount()
+	{
+		return count($this->_population);
+	}
+	
+	public function growPopulation()
+	{
+		list($male, $female) = $this->getFittest();
+		
+		while($this->getPopulationCount() < POPULATION_SIZE)
+		{
+			$child = $male->shag($female);
+			$this->addNewChild($child);
+		}
+		
+		$this->sortByFitness();
+	}
+	
+	public function addNewChild(Organism $child)
+	{
+		$this->_population[] = $child;
+	}
+	
+	public function age()
+	{
+		foreach ($this->_population as $organism) {
+			$organism->incrementAge();
 		}
 	}
 }
 
-function removeFittest(&$population){
-  $fittest = '';
-  $fittestKey = null;
-  foreach ($population as $key => $string){
-    if (fitness($string, TARGET) < fitness($fittest, TARGET)){
-      $fittest = $string;
-      $fittestKey = $key;
-    }
-  }
-  unset($population[$fittestKey]);
-  return $fittest;
-}
 
-function newPopulation($female, $male, $size){
-  $population = array();
-  for ($i = 0; $i < $size; $i++){
-    $population[] = sex($female, $male, MUTATION_CHANCE);
-  }
-  return $population;
-} 
 
-function sex($female, $male, $mutationChance){
-  $child  = '';
-  $length = strLen($female);
-  $female = str_split($female);
-  $male   = str_split($male);
-  $mutationChance = intval(1/$mutationChance);
-
-  for ($i = 0; $i < $length; $i++){
-    if (rand(0,$mutationChance) == 0){
-      $gene = randomGene();
-    } else if(rand(0, 1)) {
-      $gene = $female[$i];
-    } else {
-      $gene = $male[$i];
-    }
-    $child .= $gene;
-  }
-  return $child;
-}
-
+$population = new Population(POPULATION_SIZE);
 $generation = 0;
+
+
 do {
   $generation++;
-  $eliteFemale = removeFittest($population);
-  $eliteMale   = removeFittest($population);
-  $population  = newPopulation($eliteFemale, $eliteMale, POPULATION_SIZE);
+  list($male, $female) = $population->getFittest();
 
+  $population->cull();
+  $population->growPopulation();
+  $population->age();
+  
   if (!($generation % 10)){
     echo "\nGeneration {$generation}\n";
-    echo "Elite female: {$eliteFemale}\n";
-    echo "Elite male:   {$eliteMale}\n";
+    echo "Elite male:   {$male->getGenes()}\n";
+    echo "Elite female: {$female->getGenes()}\n";
+    echo "Population Size: {$population->getPopulationCount()}\n";
   }
-} while(fitness($eliteFemale, TARGET));
+} while($male->getFitness());
 
 echo "\nMet target at generation {$generation} with:\n";
-echo "Elite female: {$eliteFemale}\n";
-echo "Elite male:   {$eliteMale}\n";
+echo "Elite male:   {$female->getGenes()}\n";
+echo "Elite female: {$male->getGenes()}\n";
 
 
